ed9bd7fc8a1df10522aa91fb6b58b90dbe95b366
Ventsislav Nikolov <vencislaf@gmail.com>
2015-09-16 21:10:13 +0300

    Add few generated phoenix apps

---
+++ generated/v1.0.2/lib/phoenix_diff/repo.ex
@@ -0,0 +1,3 @@
+defmodule PhoenixDiff.Repo do
+  use Ecto.Repo, otp_app: :phoenix_diff
+end
---
+++ generated/v1.0.2/test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixDiff.PageControllerTest do
+  use PhoenixDiff.ConnCase
+
+  test "GET /" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
